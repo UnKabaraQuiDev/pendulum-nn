57fd7d8f4ab570827eae7cf2359f4228783a2795
UnKabaraQuiDev <66266372+UnKabaraQuiDev@users.noreply.github.com>
2026-03-21 20:05:38 +0100

    Updated

--- src/main/java/lu/kbra/pendulum_nn/LimitedInstanceEmitter.java
+++ src/main/java/lu/kbra/pendulum_nn/LimitedInstanceEmitter.java
@@ -3,7 +3,7 @@ package lu.kbra.pendulum_nn;
 import java.util.function.IntFunction;
 
 import lu.kbra.standalone.gameengine.cache.attrib.Mat4fAttribArray;
-import lu.kbra.standalone.gameengine.cache.attrib.impl.AttribArray;
+import lu.kbra.standalone.gameengine.cache.attrib.impl.JavaAttribArray;
 import lu.kbra.standalone.gameengine.geom.Mesh;
 import lu.kbra.standalone.gameengine.geom.instance.InstanceEmitter;
 import lu.kbra.standalone.gameengine.utils.transform.Transform;
@@ -12,17 +12,17 @@ public class LimitedInstanceEmitter extends InstanceEmitter {
 
 	protected int particleCount;
 
-	public LimitedInstanceEmitter(String name, Mesh mesh, int count, IntFunction<Transform> baseTransform, AttribArray... attribs) {
+	public LimitedInstanceEmitter(String name, Mesh mesh, int count, IntFunction<Transform> baseTransform, JavaAttribArray... attribs) {
 		super(name, mesh, count, baseTransform, attribs);
 		this.particleCount = count;
 	}
 
-	public LimitedInstanceEmitter(String name, Mesh mesh, int count, Transform baseTransform, AttribArray... attribs) {
+	public LimitedInstanceEmitter(String name, Mesh mesh, int count, Transform baseTransform, JavaAttribArray... attribs) {
 		super(name, mesh, count, baseTransform, attribs);
 		this.particleCount = count;
 	}
 
-	public LimitedInstanceEmitter(String name, Mesh mesh, int count, Mat4fAttribArray baseTransform, AttribArray... attribs) {
+	public LimitedInstanceEmitter(String name, Mesh mesh, int count, Mat4fAttribArray baseTransform, JavaAttribArray... attribs) {
 		super(name, mesh, count, baseTransform, attribs);
 		this.particleCount = count;
 	}
